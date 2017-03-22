5e23261c313d1c78e1052bdd595fabb87c325832
Katherine Wyman <katherine.i.wyman@gmail.com>
2017-03-21 18:18:56 -0700

    changed bootstrap theme up

---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#hide-flash").click(function(){
+        $(".flash-message").hide();
+    });
+});
---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.master')
+
+@section('main_content')
+    <div class="jumbotron">
+        <div class="jumbotron_content">
+            <h1>Welcome to Big and Little Cat Rescue</h1>
+            <p><a href="/animals" class="btn btn-primary btn-lg">Meet the family...</a></p>
+        </div>
+    </div>
+    <h2>Our Mission</h2>
+    <p>To provide for a safe, permanent and loving home to exotic animals in need. CARE focuses on excellence in physical and emotional care, advocating animal welfare through education, and conducting minimally-invasive research that will improve living conditions for captive animal populations world-wide.</p>
+
+    <p><a href="#" class="btn btn-primary btn-lg">Read more about CARE...</a></p>
+
+    <h2>What's happening at CARE:...</h2>
+    <p>(current news stories)</p>
+
+    <h2>Tour Info</h2>
+    <p>Tour Hours: Every Saturday and Sunday at 11am, 1pm and 3pm. All tours are for ages 7+ </p>
+
+    <p><a href="#" class="btn btn-primary btn-lg">Find your tour here</a></p>
+
+
+@stop
